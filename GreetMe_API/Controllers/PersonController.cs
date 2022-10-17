10a9f2a9ca6a2d2de31796264be4fb05dec97c0a
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-10-17 13:07:04 +0200

    controller working

--- GreetMe_API/Controllers/PersonController.cs
+++ GreetMe_API/Controllers/PersonController.cs
@@ -13,7 +13,82 @@ using GreetMe_DataAccess.Interface;
 
 namespace GreetMe_API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
+
     public class PersonController : Controller
     {
+        private readonly IPersonRepository _personRepository;
+
+        [ActivatorUtilitiesConstructor]
+
+        public PersonController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        //-----------------------------------------------------------------------------
+        /* GetAll                                                                    */
+        //-----------------------------------------------------------------------------
+
+        //GetAll
+        [HttpGet(Name = "GetAllPeople")]
+        public IEnumerable<Person> GetAll()
+        {
+            return _personRepository.GetAll();
+        }
+
+        //GetAll Async
+        [HttpGet(Name = "GetAllPeopleAsync")]
+        public async Task<IEnumerable<Person>> GetAllAsync()
+        {
+            return await _personRepository.GetAllAsync();
+        }
+
+        //-----------------------------------------------------------------------------
+        /* Get / Read                                                                */
+        //-----------------------------------------------------------------------------
+
+        //Get By Id
+        [HttpGet("{id}")]
+        public ActionResult<PersonDto> GetById(int id)
+        {
+            //Input validator, 0 <
+            if (id <= 0)
+            {
+                return new StatusCodeResult(422);
+            }
+
+            Person? foundPerson = _personRepository.GetById(id);
+
+            //if person is found
+            if (foundPerson is not null)
+            {
+                PersonDto personDto = PersonDTOConverter.ConvertTo(foundPerson);
+                return Ok(personDto);
+            }
+
+            //if not found
+            else
+            {
+                return new StatusCodeResult(404);
+            }
+        }
+
+        //-----------------------------------------------------------------------------
+        /* Create / Post                                                              */
+        //-----------------------------------------------------------------------------
+
+
+
+        //-----------------------------------------------------------------------------
+        /* Update                                                                    */
+        //-----------------------------------------------------------------------------
+
+
+
+        //-----------------------------------------------------------------------------
+        /* Delete                                                                    */
+        //-----------------------------------------------------------------------------
     }
 }
