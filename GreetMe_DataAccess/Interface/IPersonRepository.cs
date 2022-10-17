9336c8645ffc4366bed8c35d047abbcb8c839e63
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-10-17 12:02:55 +0200

    irepository and repository

--- GreetMe_DataAccess/Interface/IPersonRepository.cs
+++ GreetMe_DataAccess/Interface/IPersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GreetMe_DataAccess.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GreetMe_DataAccess.Interface
 {
-    public interface IPersonRepository
+    public interface IPersonRepository : IRepository<Person>, IAsyncRepository<Person>
     {
         //-----------------------------------------------------------------------------
         /* GetAll                                                                    */
