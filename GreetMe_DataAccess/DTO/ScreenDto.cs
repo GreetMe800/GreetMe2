1fdcb94253622938716cb5928e88445c964345c4
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-10-17 10:41:23 +0200

    DTO's setup

---
+++ GreetMe_DataAccess/DTO/ScreenDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetMe_DataAccess.DTO
+{
+    public class ScreenDto
+    {
+        public int Id { get; set; }
+        public string ScreenName { get; set; }
+
+        public ScreenDto()
+        {
+
+        }
+
+        public ScreenDto(int id, string screenName)
+        {
+            Id = id;
+            ScreenName = screenName;
+        }
+    }
+}
