e04ae109eaa03ba4c48533337d0c4e73c6f16de2
Martin L. S <Starknown@gmail.com>
2022-10-28 10:48:37 +0200

    ViewController API WIP

---
+++ GreetMe3/GreetMe_API/Controllers/ViewController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GreetMe_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ViewController : Controller
+    {
+
+        //-----------------------------------------------------------------------------
+        /* Get / Read                                                                */
+        //-----------------------------------------------------------------------------
+
+        [HttpGet]
+        [Route("get/{id}")]
+        public ActionResult GetById(int id)
+        {
+            //Input Validator, 0 <
+            if (id <= 0)
+            {
+                return new StatusCodeResult(422);
+            }
+            View? foundView = _viewRepository.GetById(id);
+
+            if (foundView is not null)
+            {
+                ViewDto viewDto = ViewDTOConverter.ConvertTo(foundView);
+                return Ok(viewDto);
+            }
+
+            else
+            {
+                return new StatusCodeResult(404);
+            }
+        }
+
+        [HttpGet, Route("viewName")]
+        public ActionResult<ViewDto> GetByViewName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //-----------------------------------------------------------------------------
+        /* Create / Post                                                              */
+        //-----------------------------------------------------------------------------
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] ViewDto viewDto)
+
+            //-----------------------------------------------------------------------------
+            /* Update                                                                    */
+            //-----------------------------------------------------------------------------
+
+            //-----------------------------------------------------------------------------
+            /* Delete                                                                    */
+            //-----------------------------------------------------------------------------
+        }
+}
