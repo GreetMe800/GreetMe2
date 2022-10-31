c762e71af5b8d51248d301ce9d053fd13b59f40a
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-10-31 14:31:16 +0100

    scripts updated for datbase first, and app config added back

--- GreetMe3/locally_create_database_and_tables.sql
+++ GreetMe3/locally_create_database_and_tables.sql
@@ -11,18 +11,12 @@ GO
 USE [WEXO_GreetMe]
 GO
 
-CREATE TABLE [dbo].[people](
-	[id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	[full_name] [nvarchar](50) NOT NULL,
-	[date_of_birth] [date] NOT NULL,
-	[hiring_date] [date] NOT NULL,
-	[email] [nvarchar](50) UNIQUE NOT NULL
-	)
-GO
-
 CREATE TABLE [dbo].[views](
 	[id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	[view_name] [nvarchar](50) NOT NULL
+	[view_name] [nvarchar](50) NOT NULL,
+	[layout_id] [int] NOT NULL,
+
+	CONSTRAINT [FK_view_layout_id] FOREIGN KEY (layout_id) REFERENCES [views] (id),
 	)
 GO
 
@@ -38,16 +32,40 @@ CREATE TABLE [dbo].[components](
 )
 GO
 
-CREATE TABLE [dbo].[view_layout_components](
-	[view_id] [int] NOT NULL,
-	[layout_id] [int] NOT NULL,
+CREATE TABLE [dbo].[component_positions](
+	[id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[component_name] [nvarchar](50) NOT NULL,
 	[component_id] [int] NOT NULL,
-	[component_position] [int] NOT NULL,
 
-	CONSTRAINT [FK_view_layout_components_view_id] FOREIGN KEY (view_id) REFERENCES [views] (id),
-	CONSTRAINT [FK_view_layout_components_layout_id] FOREIGN KEY (layout_id) REFERENCES [layouts] (id),
-	CONSTRAINT [FK_view_layout_components_component_id] FOREIGN KEY (component_id) REFERENCES [components] (id)
+	CONSTRAINT [FK_component_position_components_id] FOREIGN KEY (component_id) REFERENCES [components] (id)
 )
+GO
+
+CREATE TABLE [dbo].[component_views](
+	[component_id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[view_id] [int] NOT NULL,
+
+	CONSTRAINT [FK_component_views_components_id] FOREIGN KEY (component_id) REFERENCES [components] (id),
+	CONSTRAINT [FK_component_views_views_id] FOREIGN KEY (component_id) REFERENCES [views] (id)
+)
+GO
+
+CREATE TABLE [dbo].[component_component_positions](
+	[component_id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[component_position_id] [int] NOT NULL,
+
+	CONSTRAINT [FK_component_component_positions_components_id] FOREIGN KEY (component_id) REFERENCES [components] (id),
+	CONSTRAINT [FK_component_component_positions_component_positions_id] FOREIGN KEY (component_position_id) REFERENCES [component_positions] (id)
+)
+GO
+
+CREATE TABLE [dbo].[people](
+	[id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[full_name] [nvarchar](50) NOT NULL,
+	[date_of_birth] [date] NOT NULL,
+	[hiring_date] [date] NOT NULL,
+	[email] [nvarchar](50) UNIQUE NOT NULL
+	)
 GO
 
 CREATE TABLE [dbo].[menus](
--- GreetMe3/locally_drop_database_and_tables.sql
+++ GreetMe3/locally_drop_database_and_tables.sql
@@ -5,25 +5,31 @@ GO
 
 --DROP FK's
 
-ALTER TABLE [dbo].[view_layout_components] DROP CONSTRAINT [FK_view_layout_components_view_id]
+ALTER TABLE [dbo].[layouts] DROP CONSTRAINT [FK_component_component_positions_component_positions_id]
 GO
-ALTER TABLE [dbo].[view_layout_components] DROP CONSTRAINT [FK_view_layout_components_layout_id]
+ALTER TABLE [dbo].[component_positions] DROP CONSTRAINT [FK_component_component_positions_components_id]
 GO
-ALTER TABLE [dbo].[view_layout_components] DROP CONSTRAINT [FK_view_layout_components_component_id]
+ALTER TABLE [dbo].[component_views] DROP CONSTRAINT [FK_component_views_views_id]
+GO
+ALTER TABLE [dbo].[component_views] DROP CONSTRAINT [FK_component_views_components_id]
+GO
+ALTER TABLE [dbo].[component_component_positions] DROP CONSTRAINT [FK_component_position_components_id]
+GO
+ALTER TABLE [dbo].[component_component_positions] DROP CONSTRAINT [FK_view_layout_id]
 GO
 
 --DROP TABLES
-DROP TABLE [dbo].[menus]
-GO
-DROP TABLE [dbo].[view_layout_components]
-GO
-DROP TABLE [dbo].[components]
+DROP TABLE [dbo].[views]
 GO
 DROP TABLE [dbo].[layouts]
 GO
-DROP TABLE [dbo].[views]
+DROP TABLE [dbo].[components]
+GO
+DROP TABLE [dbo].[component_positions]
 GO
 DROP TABLE [dbo].[people]
+GO
+DROP TABLE [dbo].[menus]
 GO
 
 /* DROP WEXO_GREET_ME DATABASE */
