6a7ab46c898684b15dacf429084a0464ae86f3dc
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-10-31 14:37:14 +0100

    interface and repository uncommited, tp insure database/code first principals can work

--- GreetMe3/GreetMe_DataAccess/Repository/ComponentRepository.cs
+++ GreetMe3/GreetMe_DataAccess/Repository/ComponentRepository.cs
@@ -1,95 +1,95 @@
-﻿using GreetMe_DataAccess.DataAccess;
-using GreetMe_DataAccess.Interface;
-using GreetMe_DataAccess.Model;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using GreetMe_DataAccess.DataAccess;
+//using GreetMe_DataAccess.Interface;
+//using GreetMe_DataAccess.Model;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace GreetMe_DataAccess.Repository
-{
-    public class ComponentRepository : IComponentRepository
-    {
+//namespace GreetMe_DataAccess.Repository
+//{
+//    public class ComponentRepository : IComponentRepository
+//    {
 
-        //ConnectionString
-        private readonly ComponentContext _db;
-        public ComponentRepository()
-        {
-            _db = new ComponentContext();
-        }
+//        //ConnectionString
+//        private readonly ComponentContext _db;
+//        public ComponentRepository()
+//        {
+//            _db = new ComponentContext();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* GetAll / Read                                                             */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* GetAll / Read                                                             */
+//        //-----------------------------------------------------------------------------
 
-        public IEnumerable<Component> GetAll()
-        {
-            var components = _db.Components;
-            return components.ToList();
-        }
+//        public IEnumerable<Component> GetAll()
+//        {
+//            var components = _db.Components;
+//            return components.ToList();
+//        }
 
-        public async Task<IEnumerable<Component>> GetAllAsync()
-        {
-            return await _db.Components.ToListAsync();
-        }
+//        public async Task<IEnumerable<Component>> GetAllAsync()
+//        {
+//            return await _db.Components.ToListAsync();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Get / Read                                                                */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Get / Read                                                                */
+//        //-----------------------------------------------------------------------------
 
-        public Component? Get(int id)
-        {
-            return _db.Components.FirstOrDefault(p => p.Id == id);
-        }
+//        public Component? Get(int id)
+//        {
+//            return _db.Components.FirstOrDefault(p => p.Id == id);
+//        }
 
-        public async Task<Component?> GetAsync(int id)
-        {
-            return await _db.Components.FirstOrDefaultAsync(p => p.Id == id);
-        }
+//        public async Task<Component?> GetAsync(int id)
+//        {
+//            return await _db.Components.FirstOrDefaultAsync(p => p.Id == id);
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Create / Post                                                             */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Create / Post                                                             */
+//        //-----------------------------------------------------------------------------
 
-        public bool Create(Component entity)
-        {
-            throw new NotImplementedException();
+//        public bool Create(Component entity)
+//        {
+//            throw new NotImplementedException();
 
-        }
+//        }
 
-        public async Task<bool> CreateAsync(Component entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async Task<bool> CreateAsync(Component entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Update / Put                                                              */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Update / Put                                                              */
+//        //-----------------------------------------------------------------------------
 
-        public bool Update(Component entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public bool Update(Component entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async Task<bool> UpdateAsync(Component entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async Task<bool> UpdateAsync(Component entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Delete / Remove                                                           */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Delete / Remove                                                           */
+//        //-----------------------------------------------------------------------------
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public bool Delete(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async Task<bool> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public async Task<bool> DeleteAsync(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
--- GreetMe3/GreetMe_DataAccess/Repository/MenuRepository.cs
+++ GreetMe3/GreetMe_DataAccess/Repository/MenuRepository.cs
@@ -1,95 +1,95 @@
-﻿using GreetMe_DataAccess.Interface;
-using GreetMe_DataAccess.Model;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GreetMe_DataAccess.DataAccess;
+﻿//using GreetMe_DataAccess.Interface;
+//using GreetMe_DataAccess.Model;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using GreetMe_DataAccess.DataAccess;
 
-namespace GreetMe_DataAccess.Repository
-{
-    public class MenuRepository : IMenuRepository
-    {
+//namespace GreetMe_DataAccess.Repository
+//{
+//    public class MenuRepository : IMenuRepository
+//    {
 
-        //ConnectionString
-        private readonly MenuContext _db;
-        public MenuRepository()
-        {
-            _db = new MenuContext();
-        }
+//        //ConnectionString
+//        private readonly MenuContext _db;
+//        public MenuRepository()
+//        {
+//            _db = new MenuContext();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* GetAll / Read                                                             */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* GetAll / Read                                                             */
+//        //-----------------------------------------------------------------------------
 
-        public IEnumerable<Menu> GetAll()
-        {
-            var menus = _db.Menus;
-            return menus.ToList();
-        }
+//        public IEnumerable<Menu> GetAll()
+//        {
+//            var menus = _db.Menus;
+//            return menus.ToList();
+//        }
 
-        public async Task<IEnumerable<Menu>> GetAllAsync()
-        {
-            return await _db.Menus.ToListAsync();
-        }
+//        public async Task<IEnumerable<Menu>> GetAllAsync()
+//        {
+//            return await _db.Menus.ToListAsync();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Get / Read                                                                */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Get / Read                                                                */
+//        //-----------------------------------------------------------------------------
 
-        public Menu? Get(int id)
-        {
-            return _db.Menus.FirstOrDefault(p => p.Id == id);
-        }
+//        public Menu? Get(int id)
+//        {
+//            return _db.Menus.FirstOrDefault(p => p.Id == id);
+//        }
 
-        public async Task<Menu?> GetAsync(int id)
-        {
-            return await _db.Menus.FirstOrDefaultAsync(p => p.Id == id);
-        }
+//        public async Task<Menu?> GetAsync(int id)
+//        {
+//            return await _db.Menus.FirstOrDefaultAsync(p => p.Id == id);
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Create / Post                                                             */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Create / Post                                                             */
+//        //-----------------------------------------------------------------------------
 
-        public bool Create(Menu entity)
-        {
-            throw new NotImplementedException();
+//        public bool Create(Menu entity)
+//        {
+//            throw new NotImplementedException();
 
-        }
+//        }
 
-        public async Task<bool> CreateAsync(Menu entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async Task<bool> CreateAsync(Menu entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Update / Put                                                              */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Update / Put                                                              */
+//        //-----------------------------------------------------------------------------
 
-        public bool Update(Menu entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public bool Update(Menu entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async Task<bool> UpdateAsync(Menu entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async Task<bool> UpdateAsync(Menu entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        //-----------------------------------------------------------------------------
-        /* Delete / Remove                                                           */
-        //-----------------------------------------------------------------------------
+//        //-----------------------------------------------------------------------------
+//        /* Delete / Remove                                                           */
+//        //-----------------------------------------------------------------------------
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public bool Delete(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async Task<bool> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public async Task<bool> DeleteAsync(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
