1e188da355085a0743dd2ae0e3e7ce0eb99a35d3
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-11-02 11:24:39 +0100

    testdata updated

--- GreetMe3/locally_create_testdata.sql
+++ GreetMe3/locally_create_testdata.sql
@@ -110,19 +110,19 @@ insert into layout_component_position (layout_id, component_position_id) values 
 GO
 
 --people
-insert into people (full_name, date_of_birth, hiring_date, email) values ('Reidar Holsall', '6/18/1979', '11/17/1991', 'rholsall0@moonfruit.com');
-insert into people (full_name, date_of_birth, hiring_date, email) values ('Gwennie Wethered', '6/28/1973', '4/17/2001', 'gwethered1@google.de');
-insert into people (full_name, date_of_birth, hiring_date, email) values ('Cecilius Matei', '9/10/1977', '7/11/1990', 'cmatei2@vkontakte.ru');
-insert into people (full_name, date_of_birth, hiring_date, email) values ('Fedora O''Corrigane', '10/21/1969', '2/10/1995', 'focorrigane3@usda.gov');
-insert into people (full_name, date_of_birth, hiring_date, email) values ('Milt Fawdrie', '12/8/1999', '9/6/1997', 'mfawdrie4@fema.gov');
-insert into people (full_name, date_of_birth, hiring_date, email) values ('Fidel Ruit', '10/4/1985', '2/16/2002', 'fruit5@google.com.hk');
+insert into people (full_name, date_of_birth, hiring_date, email) values ('Reidar Holsall', '11/2/1979', '11/4/1991', 'rholsall0@moonfruit.com');
+insert into people (full_name, date_of_birth, hiring_date, email) values ('Gwennie Wethered', '11/2/1973', '11/4/2001', 'gwethered1@google.de');
+insert into people (full_name, date_of_birth, hiring_date, email) values ('Cecilius Matei', '11/3/1977', '11/3/1990', 'cmatei2@vkontakte.ru');
+insert into people (full_name, date_of_birth, hiring_date, email) values ('Fedora O''Corrigane', '11/3/1969', '11/3/1995', 'focorrigane3@usda.gov');
+insert into people (full_name, date_of_birth, hiring_date, email) values ('Milt Fawdrie', '11/4/1999', '11/2/1997', 'mfawdrie4@fema.gov');
+insert into people (full_name, date_of_birth, hiring_date, email) values ('Fidel Ruit', '11/4/1985', '11/2/2002', 'fruit5@google.com.hk');
 GO
 
 --menus
-insert into menus (menu_name, start_date, end_date, description) values ('Sticky Whiteleaf Manzanita', '4/15/2023', null, 'PT87 0165 5408 2200 7189 6720 9');
-insert into menus (menu_name, start_date, end_date, description) values ('Molokai Peperomia', '1/1/2024', '1/18/2021', 'DO93 T4AH 1703 8062 3395 7062 6571');
-insert into menus (menu_name, start_date, end_date, description) values ('Sulphur-flower Buckwheat', '9/26/2024', null, 'AZ40 XNRR INVY X9NZ 4SUZ 4GG1 2RY0');
-insert into menus (menu_name, start_date, end_date, description) values ('Oneflower Fringed Gentian', '3/7/2023', null, 'SI98 4327 5007 7159 780');
-insert into menus (menu_name, start_date, end_date, description) values ('Fir Dwarf Mistletoe', '7/22/2023', '10/28/2021', 'BG74 YWQO 2032 818S 82YP UR');
-insert into menus (menu_name, start_date, end_date, description) values ('Small Evening Primrose', '1/9/2022', null, 'FR98 0068 2008 24WD FXUN ZCSH S94');
+insert into menus (menu_name, start_date, end_date, description) values ('Sticky Whiteleaf Manzanita', '11/2/2023', null, 'PT87 0165 5408 2200 7189 6720 9');
+insert into menus (menu_name, start_date, end_date, description) values ('Molokai Peperomia', '11/3/2024', '11/6/2021', 'DO93 T4AH 1703 8062 3395 7062 6571');
+insert into menus (menu_name, start_date, end_date, description) values ('Sulphur-flower Buckwheat', '11/7/2024', null, 'AZ40 XNRR INVY X9NZ 4SUZ 4GG1 2RY0');
+insert into menus (menu_name, start_date, end_date, description) values ('Oneflower Fringed Gentian', '11/8/2023', null, 'SI98 4327 5007 7159 780');
+insert into menus (menu_name, start_date, end_date, description) values ('Fir Dwarf Mistletoe', '11/9/2023', '10/15/2021', 'BG74 YWQO 2032 818S 82YP UR');
+insert into menus (menu_name, start_date, end_date, description) values ('Small Evening Primrose', '11/16/2022', null, 'FR98 0068 2008 24WD FXUN ZCSH S94');
 GO
