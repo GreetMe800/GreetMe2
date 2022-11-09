bb10ef0880552596ee5a79fb66695884c94ac262
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-11-09 10:48:37 +0100

    minor changes

--- GreetMe3/GreetMe_DataAccess/Repository/LayoutRepository.cs
+++ GreetMe3/GreetMe_DataAccess/Repository/LayoutRepository.cs
@@ -80,14 +80,6 @@ namespace GreetMe_DataAccess.Repository
             return await _db.SaveChangesAsync() != 0; //retuens true if numb of effected rows is not 0
         }
 
-        //Create (Returns object insted of bool - for test)
-        public Layout CreateTest(Layout entity)
-        {
-            _db.Layouts.Add(entity);
-            _db.SaveChanges();
-            return entity;
-        }
-
         //-----------------------------------------------------------------------------
         /* Update / Put                                                              */
         //-----------------------------------------------------------------------------
