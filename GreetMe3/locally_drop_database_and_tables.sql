b95a0a3887b04cbfe22fbef36722c22ee7f6743b
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-11-10 09:51:40 +0100

    sql drop testdata and drop database script updated

--- GreetMe3/locally_drop_database_and_tables.sql
+++ GreetMe3/locally_drop_database_and_tables.sql
@@ -24,13 +24,15 @@ DROP TABLE [dbo].[menus]
 GO
 DROP TABLE [dbo].[people]
 GO
+DROP TABLE [dbo].[layout_component_position]
+GO
 DROP TABLE [dbo].[component_positions]
 GO
 DROP TABLE [dbo].[components]
 GO
-DROP TABLE [dbo].[views]
-GO
 DROP TABLE [dbo].[layouts]
+GO
+DROP TABLE [dbo].[views]
 GO
 
 /* DROP WEXO_GREET_ME DATABASE */
--- GreetMe3/locally_drop_testdata.sql
+++ GreetMe3/locally_drop_testdata.sql
@@ -1,1 +1,19 @@
-﻿
+﻿/* DROP WEXO_GREETME DATABASE */
+
+USE [WEXO_GreetMe]
+GO
+
+DELETE FROM [menus]
+GO
+DELETE FROM [people]
+GO
+DELETE FROM [layout_component_position]
+GO
+DELETE FROM [component_positions]
+GO
+DELETE FROM [components]
+GO
+DELETE FROM [layouts]
+GO
+DELETE FROM [views]
+GO
