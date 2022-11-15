15c64197bbbb02d9732fb970da15271077cb83b8
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-11-15 06:37:09 +0100

    script update

--- GreetMe3/locally_drop_database_and_tables.sql
+++ GreetMe3/locally_drop_database_and_tables.sql
@@ -5,18 +5,7 @@ GO
 
 --DROP FK's
 
-ALTER TABLE [dbo].[layout_component_position] DROP CONSTRAINT [FK_layout_component_position_component_position_id]
-GO
-ALTER TABLE [dbo].[layout_component_position] DROP CONSTRAINT [FK_layout_component_position_layout_id]
-GO
-ALTER TABLE [dbo].[layout_component_position] DROP CONSTRAINT [PK_layout_component_position]
-GO
-ALTER TABLE [dbo].[component_view] DROP CONSTRAINT [PK_component_view]
-GO
-ALTER TABLE [dbo].[component_positions] DROP CONSTRAINT [FK_component_position_component_id]
-GO
-ALTER TABLE [dbo].[components] DROP CONSTRAINT [FK_view_layout_id]
-GO
+
 
 --DROP TABLES
 
@@ -24,13 +13,7 @@ DROP TABLE [dbo].[menus]
 GO
 DROP TABLE [dbo].[people]
 GO
-DROP TABLE [dbo].[component_positions]
-GO
-DROP TABLE [dbo].[components]
-GO
 DROP TABLE [dbo].[views]
-GO
-DROP TABLE [dbo].[layouts]
 GO
 
 /* DROP WEXO_GREET_ME DATABASE */
