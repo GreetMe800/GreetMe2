703406941d44f49b6b390c309bac80783487a989
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-11-20 09:55:18 +0100

    hides time from datetime to front end

--- GreetMe3/GreetMe_MVC/Models/MenuViewModel.cs
+++ GreetMe3/GreetMe_MVC/Models/MenuViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GreetMe_API.DTO;
+using System.ComponentModel.DataAnnotations;
 
 namespace GreetMe_MVC.Models
 {
@@ -6,7 +7,9 @@ namespace GreetMe_MVC.Models
     {
         public int Id { get; set; }
         public string MenuName { get; set; } = null!;
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
         public string Description { get; set; } = null!;
     }
--- GreetMe3/GreetMe_MVC/Models/PersonViewModel.cs
+++ GreetMe3/GreetMe_MVC/Models/PersonViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GreetMe_API.DTO;
+using System.ComponentModel.DataAnnotations;
 
 namespace GreetMe_MVC.Models
 {
@@ -6,7 +7,9 @@ namespace GreetMe_MVC.Models
     {
         public int Id { get; set; }
         public string FullName { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+        [DataType(DataType.Date)]
         public DateTime HiringDate { get; set; }
         public string Email { get; set; }
     }
