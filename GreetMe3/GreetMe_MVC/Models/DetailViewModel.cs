dbe4e16774d73f01b26eedf49c5f3b15da5c3a31
StjernRogue <35777208+StjernRogue@users.noreply.github.com>
2022-11-22 21:30:03 +0100

    Test

---
+++ GreetMe3/GreetMe_MVC/Models/DetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GreetMe_API.DTO;
+
+namespace GreetMe_MVC.Models
+{
+    public class DetailViewModel
+    {
+
+        public int Id { get; set; }
+        public string ViewName { get; set; }
+        public bool HasBirthday { get; set; }
+        public bool HasAnniversary { get; set; }
+        public List<PersonDto> BirthdaysToday { get; set; }
+        public List<PersonDto> AnniversariesToday { get; set; }
+
+        public DetailViewModel(ViewDto viewDto)
+        {
+            HasBirthday = viewDto.HasBirthday;
+            HasAnniversary = viewDto.HasAnniversary;
+            ViewName = viewDto.ViewName;
+            BirthdaysToday = viewDto.BirthdaysToday;
+            AnniversariesToday = viewDto.AnniversariesToday;
+        }
+    }
+}
