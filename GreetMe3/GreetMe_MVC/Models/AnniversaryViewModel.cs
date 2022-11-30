eb0fd33db591912e3e797b16676ac2d321ad569f
Martin L. S <Starknown@gmail.com>
2022-11-30 09:33:54 +0100

    ViewModelsAdded

---
+++ GreetMe3/GreetMe_MVC/Models/AnniversaryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GreetMe_API.DTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreetMe_MVC.Models
+{
+    public class AnniversaryViewModel : IDisplayItem
+    {
+        //Lav PersonDto med parametre til anniversary + birthday view model.
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime HiringDate { get; set; }
+        public string DisplayText { get => FullName; set => throw new NotImplementedException(); }
+        public string SubText { get => HiringDate.ToString(); set => throw new NotImplementedException(); }
+    }
+}
