b48f9356e65e968d5670af2773fdb6d279b58387
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-11-30 08:52:14 +0100

    mvc

--- GreetMe3/GreetMe_MVC/Controllers/PeopleController.cs
+++ GreetMe3/GreetMe_MVC/Controllers/PeopleController.cs
@@ -9,32 +9,146 @@ namespace GreetMe_MVC.Controllers
         {
 
         }
-        public async Task<IActionResult> Index()
+
+        //-----------------------------------------------------------------------------
+        /* Index Page & Getall                                                       */
+        //-----------------------------------------------------------------------------
+
+        //Will use person that has the same name as the method (./Person/Index)
+        //Person Index Page
+
+        /* Getall */
+        //Index Page / Getall Method 
+        //Route: ./People
+        public async Task<ActionResult> Index()
         {
             IEnumerable<PersonViewModel> PersonList = null;
+
+            //Establish Connection
             var client = ApiHelper.InitializeClient("http://localhost:5184/api/");
-            var responseTask = await client.GetAsync("person");
+
+            //HTTP GETALL
+            var responseTask = await client.GetAsync("person" + "/getall");
             if (responseTask.IsSuccessStatusCode)
             {
-                var PersonViewModelList = await responseTask.Content.ReadAsAsync<IList<PersonViewModel>>();
-
-                PersonList = PersonViewModelList;
+                var personPersonModelList = await responseTask.Content.ReadAsAsync<IList<PersonViewModel>>();
+                PersonList = personPersonModelList;
             }
-            else //web api sent error response 
+            else // If Error
             {
-                //log response status here..
-
                 PersonList = Enumerable.Empty<PersonViewModel>();
-
                 ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
             }
 
+            //Return
             return View(PersonList);
         }
 
-        public IActionResult Create()
+        //-----------------------------------------------------------------------------
+        /* Person Page & Get                                                           */
+        //-----------------------------------------------------------------------------
+
+        /* Get */
+        //Route: ./People/Details/{id}
+        public async Task<ActionResult> Details(int id)
+        {
+            PersonViewModel person = null;
+
+            //Establish Connection
+            var client = ApiHelper.InitializeClient("http://localhost:5184/api/");
+
+            //HTTP GET
+            var responseTask = await client.GetAsync("person/" + id.ToString());
+            if (responseTask.IsSuccessStatusCode)
+            {
+                var readTask = responseTask.Content.ReadAsAsync<PersonViewModel>();
+            }
+
+            return View(person);
+        }
+
+        //-----------------------------------------------------------------------------
+        /* Create Page & Create                                                      */
+        //-----------------------------------------------------------------------------
+
+        //Page
+        //Route: ./People/Create
+        public ActionResult Create()
         {
             return View();
+        }
+
+        //Method
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(PersonViewModel person)
+        {
+            //Establish Connection
+            var client = ApiHelper.InitializeClient("http://localhost:5184/api/");
+
+            //HTTP POST
+            var postTask = await client.PostAsJsonAsync<PersonViewModel>("person", person);
+            if (postTask.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            //Return
+            return View(person);
+        }
+
+        //-----------------------------------------------------------------------------
+        /* Edit Page/Get & Update                                                      */
+        //-----------------------------------------------------------------------------
+
+        //Page
+        //Route: ./People/Edit
+        public async Task<ActionResult> Edit(int id)
+        {
+            PersonViewModel person = null;
+
+            //Establish Connection
+            var client = ApiHelper.InitializeClient("http://localhost:5184/api/");
+
+            //HTTP GET
+            var responseTask = await client.GetAsync("person/" + id.ToString());
+            if (responseTask.IsSuccessStatusCode)
+            {
+                person = await responseTask.Content.ReadAsAsync<PersonViewModel>();
+            }
+
+            return View(person);
+        }
+
+        //Method
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(PersonViewModel person)
+        {
+            //Establish Connection
+            var client = ApiHelper.InitializeClient("http://localhost:5184/api/");
+
+            //HTTP PUT
+            var putTask = await client.PutAsJsonAsync<PersonViewModel>("person", person);
+            if (putTask.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        //-----------------------------------------------------------------------------
+        /* Delete Page & Create                                                      */
+        //-----------------------------------------------------------------------------
+
+        /* Delete */
+        //Route: ./People/Delete/{id}
+        public /*async*/ Task<ActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
