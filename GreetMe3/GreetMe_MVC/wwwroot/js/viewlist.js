380f0d5b5f6c46293bc335f63e5d81a05e60f0c7
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-12-13 09:31:57 +0100

    signal r minor bug fix

--- GreetMe3/GreetMe_MVC/wwwroot/js/viewlist.js
+++ GreetMe3/GreetMe_MVC/wwwroot/js/viewlist.js
@@ -37,19 +37,18 @@ function BindViewsToGrid(viewList) {
         //hasBirthday
         if (view.hasBirthday) {
             tr.append(`<td><input type="checkbox" checked disabled></td>`)
-        } else {
+        }
+        else {
             tr.append(`<td><input type="checkbox" disabled></td>`)
         }
 
         //hasAnniversary
         if (view.hasAnniversary) {
             tr.append(`<td><input type="checkbox" checked disabled></td>`)
-        } else {
+        }
+        else {
             tr.append(`<td><input type="checkbox" disabled></td>`)
         }
-        }
-
-        tr.append(`<td><input type="checkbox"` + BoolCheck(view.hasAnniversary) + `disabled></td>`)
 
         //tr.append(`<td>| <a href="/Controllername/ActionName/${object.id}">Display</a></td>`);
         tr.append(`<td><a href="/Views/Display/${view.id}">Display</a> | <a href="/Views/Details/${view.id}">Details</a> | <a href="/Views/Edit/${view.id}">Edit</a> | <a href="/Views/Delete/${view.id}">Delete</a></td>`);
