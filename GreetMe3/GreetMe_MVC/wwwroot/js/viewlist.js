b8319786ea173fd3529b77f7a5175eabf3beb669
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-12-13 09:12:31 +0100

    signal r checkbox

--- GreetMe3/GreetMe_MVC/wwwroot/js/viewlist.js
+++ GreetMe3/GreetMe_MVC/wwwroot/js/viewlist.js
@@ -24,6 +24,7 @@ connection.on("ReceivedViews", function (viewList) {
 
 });
 
+//Viewlist
 function BindViewsToGrid(viewList) {
     $('#tableViews tbody').empty();
 
@@ -32,8 +33,23 @@ function BindViewsToGrid(viewList) {
         tr = $('<tr/>');
         tr.append(`<th scope="row">${(index + 1)}</th>`)
         tr.append(`<td>${(view.viewName)}</td>`)
-        tr.append(`<td>${(view.hasBirthday)}</td>`)
-        tr.append(`<td>${(view.hasAnniversary)}</td>`)
+
+        //hasBirthday
+        if (view.hasBirthday) {
+            tr.append(`<td><input type="checkbox" checked disabled></td>`)
+        } else {
+            tr.append(`<td><input type="checkbox" disabled></td>`)
+        }
+
+        //hasAnniversary
+        if (view.hasAnniversary) {
+            tr.append(`<td><input type="checkbox" checked disabled></td>`)
+        } else {
+            tr.append(`<td><input type="checkbox" disabled></td>`)
+        }
+        }
+
+        tr.append(`<td><input type="checkbox"` + BoolCheck(view.hasAnniversary) + `disabled></td>`)
 
         //tr.append(`<td>| <a href="/Controllername/ActionName/${object.id}">Display</a></td>`);
         tr.append(`<td><a href="/Views/Display/${view.id}">Display</a> | <a href="/Views/Details/${view.id}">Details</a> | <a href="/Views/Edit/${view.id}">Edit</a> | <a href="/Views/Delete/${view.id}">Delete</a></td>`);
@@ -45,3 +61,10 @@ function BindViewsToGrid(viewList) {
 //@Html.ActionLink("Details", "Details", new { id=item.Id }) |
 //@Html.ActionLink("Edit", "Edit", new { id=item.Id }) |
 //@Html.ActionLink("Delete", "Delete", new { id=item.Id })
+
+//<div class="form-group form-check mb-3">
+//    <label class="form-check-label">
+//        <input class="form-check-input" asp-for="HasBirthday" />
+//                    @Html.DisplayNameFor(model => model.HasBirthday)
+//    </label>
+//</div>
