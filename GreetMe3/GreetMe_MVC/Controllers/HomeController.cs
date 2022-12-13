1940fb2cde47c1887c7e1d86b71f3c34f82d9c42
Allan <76095439+ZenCrash@users.noreply.github.com>
2022-12-13 10:43:48 +0100

    sprint 4 merge into main branch

--- GreetMe3/GreetMe_MVC/Controllers/HomeController.cs
+++ GreetMe3/GreetMe_MVC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace GreetMe_MVC.Controllers
 {
-    //Example Coe
+    //Example Core
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
